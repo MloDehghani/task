4291d91e93eeffcb93b04b8e226379834d9cdaa4
MloDehghani <amirdehghani137722@gmail.com>
2021-06-15 08:58:25 +0430

    add color plate

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,27 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      main:{
+        set:'#06C78D',
+        low:"#B3E8DE",
+        lowest: '#E8F8F5'
+      },
+
+      gray: {
+        15: '#FAFBFC',
+        25: '#F5F7FA',
+        50: '#E9EDF5',
+        100: '#D0DDEC',
+        200: '#AEC1DA',
+        300: '#92A7C1',
+        400: '#7B93AF',
+        500: '#6783A0',
+        600: '#57728E',
+        700: '#445A74',
+        800: '#30445B'
+      }
+    },
     extend: {},
   },
   variants: {
